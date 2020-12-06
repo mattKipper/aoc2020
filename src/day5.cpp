848274fd7c5bc0e9782a8dd8c39e32cddadb07fd
Matthew Kipper <kipper@ualberta.ca>
2020-12-06 09:49:20 -0700

    Day 5 + 6 (all parts)

---
+++ src/day5.cpp
@@ -0,0 +1,67 @@
+//
+// Created by kipper on 2020-12-05.
+//
+#include "cli.h"
+
+int seat_id(int row, int column) {
+    return (row * 8) + column;
+}
+
+int partition(const char* seat, int length, int chars, char lower, char higher) {
+    int low = 0, high = length;
+    for (size_t i = 0; i < chars; ++i) {
+        auto mid = low + (high - low) / 2;
+        if (seat[i] == lower) {
+            high = mid;
+        }
+        else if (seat[i] == higher) {
+            low = mid;
+        }
+        else {
+            return -1;
+        }
+    }
+    return low + (high - low) / 2;
+
+}
+
+int row(const std::string& seat) {
+    return partition(seat.c_str(), 128, 7, 'F', 'B');
+}
+
+int column(const std::string& seat) {
+    return partition(seat.c_str() + 7, 8, 3, 'L', 'R');
+}
+
+std::vector<bool> occupancy_list(int rows, int columns) {
+    return std::vector<bool>(rows * columns, false);
+}
+
+int solve_puzzle(const CLIInput<std::vector<std::string>>& input) {
+    if (input.part == Part::ONE) {
+        int max_id = 0;
+        for (const auto &seat: input.data) {
+            auto id = seat_id(row(seat), column(seat));
+            if (id > max_id) {
+                max_id = id;
+            }
+        }
+        return max_id;
+    }
+    else {
+        auto occupied = occupancy_list(128, 8);
+        for (const auto& seat: input.data) {
+            occupied[row(seat) * 8 + column(seat)] = true;
+        }
+        for (int i = 1; i < occupied.size(); ++i) {
+            if (!occupied[i] && occupied[i-1]) {
+                return seat_id(i / 8, i % 8);
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    return solve_cli<std::vector<std::string>,int>(argc, argv, parse_input_file_lines, solve_puzzle);
+}
+
---
+++ src/day6.cpp
@@ -0,0 +1,85 @@
+//
+// Created by kipper on 2020-12-06.
+//
+#include "cli.h"
+
+#include <unordered_set>
+#include <unordered_map>
+
+int solve_puzzle_part_two(const std::vector<std::string>& input) {
+    int total_count = 0;
+    int group_size = 0;
+    std::unordered_map<char,int> group_counts;
+
+    auto add_group_count = [&]() {
+        for (const auto& count: group_counts) {
+            if (count.second == group_size) {
+                ++total_count;
+            }
+        }
+    };
+
+    for (const auto& line: input) {
+        if (line.empty()) {
+            add_group_count();
+            group_counts.clear();
+            group_size = 0;
+        }
+        else {
+            for (const auto& answer: line) {
+                auto match = group_counts.find(answer);
+                if (match == group_counts.end()) {
+                    group_counts[answer] = 1;
+                }
+                else {
+                    match->second++;
+                }
+            }
+            group_size++;
+        }
+    }
+
+    // Just in case last line isn't empty
+    add_group_count();
+
+    return total_count;
+}
+
+int solve_puzzle_part_one(const std::vector<std::string>& input) {
+    int counts = 0;
+    std::unordered_set<char> group_answers;
+
+    for (const auto& line: input) {
+        if (line.empty()) {
+            counts += group_answers.size();
+            group_answers.clear();
+        }
+        else {
+            for (const auto& answer: line) {
+                group_answers.insert(answer);
+            }
+        }
+    }
+
+    // Don't forget the last group (group_answers.size() == 0) if the
+    // file ends with an empty line
+    counts += group_answers.size();
+
+    return counts;
+}
+
+int solve_puzzle(const CLIInput<std::vector<std::string>>& input) {
+    switch (input.part) {
+        case Part::ONE:
+            return solve_puzzle_part_one(input.data);
+        case Part::TWO:
+            return solve_puzzle_part_two(input.data);
+        default:
+            throw std::invalid_argument("Unexpected part argument");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    return solve_cli<std::vector<std::string>,int>(argc, argv, parse_input_file_lines, solve_puzzle);
+}
+
