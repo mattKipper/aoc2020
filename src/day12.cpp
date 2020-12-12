8afc79434f5e97ab630a2628f1f293ad23323054
Matthew Kipper <kipper@ualberta.ca>
2020-12-12 15:16:00 -0700

    Day 12 Part 1 refactor
    Use polar coordinates since they were added for Part 2. Also allows Part 1 to support turns that aren't divisible by 90

--- src/day12.cpp
+++ src/day12.cpp
@@ -38,51 +38,6 @@ Action action_from_char(char c) {
     }
 }
 
-enum class Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
-};
-
-Direction to_left(Direction facing) {
-    switch (facing) {
-        case Direction::NORTH: return Direction::WEST;
-        case Direction::EAST: return Direction::NORTH;
-        case Direction::SOUTH: return Direction::EAST;
-        case Direction::WEST: return Direction::SOUTH;
-    }
-}
-
-Direction to_right(Direction facing) {
-    switch (facing) {
-        case Direction::NORTH: return Direction::EAST;
-        case Direction::EAST: return Direction::SOUTH;
-        case Direction::SOUTH: return Direction::WEST;
-        case Direction::WEST: return Direction::NORTH;
-    }
-}
-
-int turn_count(int degrees) {
-    return (degrees / 90) % 4;
-}
-
-Direction turn(Direction facing, const std::function<Direction(Direction)>& turn_fn, int degrees) {
-    const auto turns = turn_count(degrees);
-    for (auto i = 0; i < turns; ++i) {
-        facing = turn_fn(facing);
-    }
-    return facing;
-}
-
-Direction turn_left(Direction orig, int degrees) {
-    return turn(orig, to_left, degrees);
-}
-
-Direction turn_right(Direction orig, int degrees) {
-    return turn(orig, to_right, degrees);
-}
-
 int magnitude_from_line(const std::string& line) {
     return std::stol(line.c_str() + 1, nullptr, 10);
 }
@@ -114,6 +69,17 @@ struct CartesianPoint {
     double y;
     CartesianPoint (double x, double y) : x(x), y(y) {}
     CartesianPoint(const PolarPoint& p);
+
+    CartesianPoint& operator+=(const CartesianPoint& rhs) {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+
+    friend CartesianPoint operator+(CartesianPoint lhs, const CartesianPoint& rhs) {
+        lhs += rhs;
+        return lhs;
+    }
 };
 
 struct PolarPoint {
@@ -136,23 +102,11 @@ PolarPoint::PolarPoint(const CartesianPoint& p) :
 
 struct Ship {
     CartesianPoint position;
-    Direction facing;
+    CartesianPoint waypoint;
+    float facing;
 
-    Ship() : position(0,0), facing(Direction::EAST) {}
+    Ship() : position(0,0), waypoint(10, 1), facing(0) {}
 };
-
-Action forward_action(Direction facing) {
-    switch (facing) {
-        case Direction::NORTH:
-            return Action::MOVE_NORTH;
-        case Direction::EAST:
-            return Action::MOVE_EAST;
-        case Direction::SOUTH:
-            return Action::MOVE_SOUTH;
-        case Direction::WEST:
-            return Action::MOVE_WEST;
-    }
-}
 
 void move_part_one(Ship& ship, const Instruction& ins) {
     switch (ins.action) {
@@ -169,77 +123,65 @@ void move_part_one(Ship& ship, const Instruction& ins) {
             ship.position.x -= ins.magnitude;
             break;
         case Action::ROTATE_LEFT:
-            ship.facing = turn_left(ship.facing, ins.magnitude);
+            ship.facing += ins.magnitude;
             break;
         case Action::ROTATE_RIGHT:
-            ship.facing = turn_right(ship.facing, ins.magnitude);
+            ship.facing -= ins.magnitude;
             break;
-        case Action::MOVE_FORWARD:
-            move_part_one(ship, Instruction(forward_action(ship.facing), ins.magnitude));
+        case Action::MOVE_FORWARD: {
+            auto offset = CartesianPoint(PolarPoint(ins.magnitude, ship.facing));
+            ship.position += offset;
+            break;
+        }
+        default:
             break;
     }
 }
 
-struct NavShip {
-    CartesianPoint ship;
-    CartesianPoint waypoint;
-    NavShip() : ship(0,0), waypoint(10,1) {}
-};
-
-void move_part_two(NavShip& nav, const Instruction& ins) {
+void move_part_two(Ship& ship, const Instruction& ins) {
     switch (ins.action) {
         case Action::MOVE_NORTH:
-            nav.waypoint.y += ins.magnitude;
+            ship.waypoint.y += ins.magnitude;
             break;
         case Action::MOVE_EAST:
-            nav.waypoint.x += ins.magnitude;
+            ship.waypoint.x += ins.magnitude;
             break;
         case Action::MOVE_SOUTH:
-            nav.waypoint.y -= ins.magnitude;
+            ship.waypoint.y -= ins.magnitude;
             break;
         case Action::MOVE_WEST:
-            nav.waypoint.x -= ins.magnitude;
+            ship.waypoint.x -= ins.magnitude;
             break;
         case Action::ROTATE_LEFT: {
-            auto waypoint_p = PolarPoint(nav.waypoint);
+            auto waypoint_p = PolarPoint(ship.waypoint);
             waypoint_p.theta = waypoint_p.theta + ins.magnitude;
-            nav.waypoint = CartesianPoint(waypoint_p);
+            ship.waypoint = CartesianPoint(waypoint_p);
             break;
         }
         case Action::ROTATE_RIGHT: {
-            auto waypoint_p = PolarPoint(nav.waypoint);
+            auto waypoint_p = PolarPoint(ship.waypoint);
             waypoint_p.theta = waypoint_p.theta - ins.magnitude;
-            nav.waypoint = CartesianPoint(waypoint_p);
+            ship.waypoint = CartesianPoint(waypoint_p);
             break;
         }
         case Action::MOVE_FORWARD:
-            nav.ship.x += (ins.magnitude * nav.waypoint.x);
-            nav.ship.y += (ins.magnitude * nav.waypoint.y);
+            ship.position.x += (ins.magnitude * ship.waypoint.x);
+            ship.position.y += (ins.magnitude * ship.waypoint.y);
             break;
     }
 }
-int solve_part_one(const std::vector<Instruction>& input) {
+
+int solve_part(const std::vector<Instruction>& input, const std::function<void(Ship&,const Instruction&)> move) {
     Ship ship;
     for (auto instruction: input) {
-        move_part_one(ship, instruction);
+        move(ship, instruction);
     }
-    return std::abs(ship.position.x) + std::abs(ship.position.y);
-}
-
-int solve_part_two(const std::vector<Instruction>& input) {
-    NavShip nav;
-    for (auto instruction: input) {
-        move_part_two(nav, instruction);
-    }
-    return std::round(std::abs(nav.ship.x) + std::abs(nav.ship.y));
+    return std::round(std::abs(ship.position.x) + std::abs(ship.position.y));
 }
 
 int solve_puzzle(const CLIInput<std::vector<Instruction>>& input) {
-    switch (input.part) {
-        case Part::ONE: return solve_part_one(input.data);
-        case Part::TWO: return solve_part_two(input.data);
-        default: return -1;
-    }
+    auto move = input.part == Part::ONE ? move_part_one : move_part_two;
+    return solve_part(input.data, move);
 }
 
 int main(int argc, char *argv[]) {
